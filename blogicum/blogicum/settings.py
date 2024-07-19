1ddbf23349f0b35a6bfdce505e3162cfe7939d7e
Пацвальд Владимир <vova.patsvald@mail.ru>
2024-07-19 11:52:41 +1000

    fix: mistake

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -12,23 +12,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3i77lr9^&*bk%qxm8!!pr-r*f6f)1gttim+#*5itvm!7yj5!f!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
